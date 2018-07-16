dededa086231485fa9f666c5f83baa46700a8d3e
Danielle Boczkowski <38707471+DanielleBK@users.noreply.github.com>
2018-07-16 13:58:52 -0300

    15.1%
    erros

--- js/app.js
+++ js/app.js
@@ -6,18 +6,3 @@ function clearDiv() {
   var clearEntrence = document.getElementById('entrence');
   clearEntrence.innerHTML='';
 }
-
-function aqp2016_2() {
-  clearDiv();
-}
-
-// for(sede in data) {
-//   console.log(sede);
-//   for(turma in data[sede]){
-//     console.log(turma)
-//     console.log(data[sede][turma].students)
-//     for (student of data[sede][turma].students){
-//       console.log(student.name)
-//     }
-//   }
-// }
